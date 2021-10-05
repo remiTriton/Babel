b405b74e22c6a709007cc77538d85b9b03d7b1bb
triton <remi.triton@epitech.eu>
2021-10-05 21:41:51 +0200

    maj

--- BabelBabel_Stock/src/components/Add.vue
+++ BabelBabel_Stock/src/components/Add.vue
@@ -1,93 +1,336 @@
 <template>
-<div class="formulaire" >
-  <form class="w-full max-w-lg">
-  <div class="flex flex-wrap -mx-1 mb-6 mt-6">
-    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-        First Name
-      </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Natural">
-      <p class="text-red-500 text-xs italic">Please fill out this field.</p>
-    </div>
-    <div class="w-full md:w-1/2 px-3">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-        Last Name
-      </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Wine">
-    </div>
-  </div>
-
-  <div class="flex flex-wrap -mx-1 mb-6">
-    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-        Wine Name
-      </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
-      <p class="text-red-500 text-xs italic">Please fill out this field.</p>
-    </div>
-    <div class="w-full md:w-1/2 px-3">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-        Color
-      </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Red, Rosé, White, Bubble">
-    </div>
-  </div>
-  
-  
-  <div class="flex flex-wrap -mx-1 mb-2">
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-        City
-      </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="...">
-    </div>
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        State
-      </label>
-      <div class="relative">
-        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-          <option>France</option>
-          <option>Italie</option>
-          <option>Espagne</option>
-          <option>Grêce</option>
-          <option>Croatie</option>
-        </select>
-        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+  <div class="formulaire">
+    <form class="w-full max-w-lg">
+      <div class="flex flex-wrap -mx-1 mb-6 mt-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            class="
+              block
+              uppercase
+              tracking-wide
+              text-gray-700 text-xs
+              font-bold
+              mb-2
+            "
+            for="grid-first-name"
+          >
+            First Name
+          </label>
+          <input
+            class="
+              appearance-none
+              block
+              w-full
+              bg-gray-200
+              text-gray-700
+              border border-red-500
+              rounded
+              py-3
+              px-4
+              mb-3
+              leading-tight
+              focus:outline-none
+              focus:bg-white
+            "
+            id="grid-first-name"
+            type="text"
+            placeholder="Natural"
+          />
+          <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+        </div>
+        <div class="w-full md:w-1/2 px-3">
+          <label
+            class="
+              block
+              uppercase
+              tracking-wide
+              text-gray-700 text-xs
+              font-bold
+              mb-2
+            "
+            for="grid-last-name"
+          >
+            Last Name
+          </label>
+          <input
+            class="
+              appearance-none
+              block
+              w-full
+              bg-gray-200
+              text-gray-700
+              border border-gray-200
+              rounded
+              py-3
+              px-4
+              leading-tight
+              focus:outline-none
+              focus:bg-white
+              focus:border-gray-500
+            "
+            id="grid-last-name"
+            type="text"
+            placeholder="Wine"
+          />
         </div>
       </div>
-    </div>
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-        Quantité
-      </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="number" placeholder="0">
-    </div>
-  </div>
-</form>
-</div>
 
+      <div class="flex flex-wrap -mx-1 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            class="
+              block
+              uppercase
+              tracking-wide
+              text-gray-700 text-xs
+              font-bold
+              mb-2
+            "
+            for="grid-first-name"
+          >
+            Wine Name
+          </label>
+          <input
+            class="
+              appearance-none
+              block
+              w-full
+              bg-gray-200
+              text-gray-700
+              border border-red-500
+              rounded
+              py-3
+              px-4
+              mb-3
+              leading-tight
+              focus:outline-none
+              focus:bg-white
+            "
+            id="grid-first-name"
+            type="text"
+            placeholder="Jane"
+          />
+          <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+        </div>
+        <div class="w-full md:w-1/2 px-3">
+          <label
+            class="
+              block
+              uppercase
+              tracking-wide
+              text-gray-700 text-xs
+              font-bold
+              mb-2
+            "
+            for="grid-last-name"
+          >
+            Color
+          </label>
+          <input
+            class="
+              appearance-none
+              block
+              w-full
+              bg-gray-200
+              text-gray-700
+              border border-gray-200
+              rounded
+              py-3
+              px-4
+              leading-tight
+              focus:outline-none
+              focus:bg-white
+              focus:border-gray-500
+            "
+            id="grid-last-name"
+            type="text"
+            placeholder="Red, Rosé, White, Bubble"
+          />
+        </div>
+      </div>
+
+      <div class="flex flex-wrap -mx-1 mb-2">
+        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+          <label
+            class="
+              block
+              uppercase
+              tracking-wide
+              text-gray-700 text-xs
+              font-bold
+              mb-2
+            "
+            for="grid-city"
+          >
+            City
+          </label>
+          <input
+            class="
+              appearance-none
+              block
+              w-full
+              bg-gray-200
+              text-gray-700
+              border border-gray-200
+              rounded
+              py-3
+              px-4
+              leading-tight
+              focus:outline-none
+              focus:bg-white
+              focus:border-gray-500
+            "
+            id="grid-city"
+            type="text"
+            placeholder="..."
+          />
+        </div>
+        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+          <label
+            class="
+              block
+              uppercase
+              tracking-wide
+              text-gray-700 text-xs
+              font-bold
+              mb-2
+            "
+            for="grid-state"
+          >
+            State
+          </label>
+          <div class="relative">
+            <select
+              class="
+                block
+                appearance-none
+                w-full
+                bg-gray-200
+                border border-gray-200
+                text-gray-700
+                py-3
+                px-4
+                pr-8
+                rounded
+                leading-tight
+                focus:outline-none
+                focus:bg-white
+                focus:border-gray-500
+              "
+              id="grid-state"
+            >
+              <option>France</option>
+              <option>Italie</option>
+              <option>Espagne</option>
+              <option>Grêce</option>
+              <option>Croatie</option>
+            </select>
+            <div
+              class="
+                pointer-events-none
+                absolute
+                inset-y-0
+                right-0
+                flex
+                items-center
+                px-2
+                text-gray-700
+              "
+            >
+              <svg
+                class="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+          <label
+            class="
+              block
+              uppercase
+              tracking-wide
+              text-gray-700 text-xs
+              font-bold
+              mb-2
+            "
+            for="grid-zip"
+          >
+            Quantité
+          </label>
+          <input
+            class="
+              appearance-none
+              block
+              w-full
+              bg-gray-200
+              text-gray-700
+              border border-gray-200
+              rounded
+              py-3
+              px-4
+              leading-tight
+              focus:outline-none
+              focus:bg-white
+              focus:border-gray-500
+            "
+            id="grid-zip"
+            type="number"
+            placeholder="0"
+          />
+        </div>
+      </div>
+      <div class="sub">
+        <button
+          type="button"
+          class="
+            inline-flex
+            items-center
+            px-4
+            py-2
+            border border-gray-300
+            shadow-sm
+            text-sm
+            font-medium
+            rounded-md
+            text-gray-700
+            bg-white
+            hover:bg-gray-50
+            focus:outline-none
+            focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+          "
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-name : "Add"
-}
+  name: "Add",
+};
 </script>
 
 <style>
-
 .formulaire {
-display : flex;
-justify-content: center;
-align-items: center;
-margin-top: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
 }
 
 .block {
-  
-justify-content: space-evenly;
+  justify-content: space-evenly;
 }
 
+.sub {
+  color: #2a574c;
+}
 </style>
