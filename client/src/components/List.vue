5ea28334e74f3334c93c27586b59bc9c29c76f43
triton <remi.triton@epitech.eu>
2021-10-21 19:55:09 +0200

    r

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -1,4 +1,41 @@
 <template>
+  <div>
+    <div class="container mt-10">
+  <div class="flex items-center">
+    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2 text-gray-600" fill="none" viewBox="0 0 24 24"
+         stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+    </svg>
+    <input type="text"  name="name" placeholder="name"
+           class="w-/10 py-2 border-b-2 border-gray-400 outline-none focus:border-green-400">
+  </div>
+
+<span>
+  <button class="test bg-transparent text-#2a574c font-bold hover:text-#2a574c  hover:border-transparent br-5 rounded">
+    Bubble
+</button></span>
+
+    <span>
+  <button class="test bg-transparent text-#2a574c  font-bold hover:text-#2a574c  hover:border-transparent br-5 rounded">
+    Red
+</button></span>
+
+    <span>
+  <button class="test bg-transparent text-#2a574c  font-bold hover:text-#2a574c  hover:border-transparent br-5 rounded">
+    White
+</button></span>
+
+    <span>
+  <button class="test bg-transparent text-#2a574c font-bold hover:text-#2a574c  hover:border-transparent br-5 rounded">
+    Rosé
+</button></span>
+
+    <span>
+  <button class="test bg-transparent text-#2a574c  font-bold hover:text-#2a574c hover:border-transparent br-5 rounded">
+    Beer
+</button></span>
+    </div>
   <div class="bg-white">
     <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <h2 class="sr-only">Products</h2>
@@ -6,7 +43,8 @@
       <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         <a v-for="product in products" :key="product.id" :href="product.href" class="group">
           <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-            <img :src="product.imageSrc" :alt="product.imageAlt" class="w-full h-full object-center object-cover group-hover:opacity-75" />
+            <img :src="product.imageSrc" :alt="product.imageAlt"
+                 class="w-full h-full object-center object-cover group-hover:opacity-75"/>
           </div>
           <h3 class="mt-4 text-sm text-gray-700">
             {{ product.name }}
@@ -17,6 +55,7 @@
         </a>
       </div>
     </div>
+  </div>
   </div>
 </template>
 
@@ -65,3 +104,21 @@ export default {
   },
 }
 </script>
+
+<style>
+.bbl {
+background-color: #2a574c;
+  text-decoration-color: #2a574c;
+}
+.test{
+  border-color: #2a574c;
+  color: #2a574c;
+}
+.container {
+  display: flex;
+  justify-content: space-evenly;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: large;
+}
+</style>
