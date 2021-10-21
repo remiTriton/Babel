180142199d3748332352b79591f9c59829f69e9b
triton <remi.triton@epitech.eu>
2021-10-21 18:42:48 +0200

    SideBar Navigation posed

---
+++ client/src/views/Admin.vue
@@ -0,0 +1,15 @@
+<template>
+<Adm />
+</template>
+
+<script>
+import Adm from "../components/Adm.vue"
+export default {
+  name: "Admin.vue",
+  components: {Adm}
+}
+</script>
+
+<style scoped>
+
+</style>
