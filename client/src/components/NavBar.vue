d5b28f35c530d095f494ec0d70e9270e6dca4520
triton <remi.triton@epitech.eu>
2021-10-25 14:34:23 +0200

    crud

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -111,6 +111,28 @@
           >Register</router-link>
         </div>
       </div>
+      <div>
+        <router-link
+            to="/Admin"
+            class="
+              cat
+              inline-block
+              text-sm
+              px-4
+              py-2
+              leading-none
+              border
+              rounded
+              text-white
+              border-white
+              hover:border-transparent
+              hover:text-teal-500
+              hover:bg-white
+              mt-4
+              lg:mt-0
+            "
+        >Admin</router-link>
+      </div>
     </nav>
   </div>
 </template>
