691c6963a4ddaf624d5c881332d0236fb80d0e8c
triton <remi.triton@epitech.eu>
2021-10-27 10:19:46 +0200

    push

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container  flex-col">
-    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+  <div class="container flex-col ">
+    <div class="-my-2  overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-2 align-middle inline-block min-w- sm:px-6 lg:px-8 justify-content-space-evenly">
         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
           <table class="min-w-full divide-y divide-gray-200">
@@ -74,8 +74,7 @@ const people = [
     name: 'Chuck is Back',
     color: 'Sparkling Red',
     domain: 'La Sorga',
-    image:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    image: 'src/images/../assets/Chuck.jpg',
   },
   // More people...
 ]
