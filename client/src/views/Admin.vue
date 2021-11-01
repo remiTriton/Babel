d72af6eadad97d413ac24162480215b1e90f3b32
triton <remi.triton@epitech.eu>
2021-11-01 15:21:43 +0100

    add btn for adm

---
+++ client/src/views/Admin.vue
@@ -0,0 +1,20 @@
+<template>
+<Adm />
+  <Dashboard />
+</template>
+
+<script>
+import Adm from "../components/Adm.vue"
+import Dashboard from "../components/Dashboard.vue"
+
+
+export default {
+  name: "Admin.vue",
+  components: {Adm, Dashboard},
+
+}
+</script>
+
+<style scoped>
+
+</style>
