bc7d3c8a3f7d2bce6e7223fb8df3d6092e94fe3d
triton <remi.triton@epitech.eu>
2021-11-01 15:24:38 +0100

    add cart btn for adm

--- client/src/components/Unique.vue
+++ client/src/components/Unique.vue
@@ -3,6 +3,7 @@
     <div
         class="max-w-2xl mx-auto py-24 px-4 grid items-center grid-cols-1 gap-y-16 gap-x-8 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
       <div>
+
         <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl -mt-24">Technical
           Specifications</h2>
         <p class="mt-4 text-gray-500">This sparkling Gamay comes from sandstone soils in the south of the Ardèche.
@@ -65,6 +66,7 @@
 
 <script>
 const features = [
+
   {name: 'Domain', description: 'South of Ardèche.'},
   {name: 'Color', description: 'Sparkling Red.'},
   {name: 'Year', description: '2017'},
