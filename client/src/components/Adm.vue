c33f722a6f52d26d474f94d10e8658f89e86ea2f
triton <remi.triton@epitech.eu>
2021-11-01 14:42:27 +0100

    push

--- client/src/components/Adm.vue
+++ client/src/components/Adm.vue
@@ -187,8 +187,8 @@ import {
 const navigation = [
   { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
   { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
+  { name: 'Import', href: '#', icon: FolderIcon, current: false },
+  { name: 'Calendar', href: '/Cal', icon: CalendarIcon, current: false },
   { name: 'Documents', href: '#', icon: InboxIcon, current: false },
   { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
 ]
--- client/src/views/Admin.vue
+++ client/src/views/Admin.vue
@@ -6,6 +6,8 @@
 <script>
 import Adm from "../components/Adm.vue"
 import Dashboard from "../components/Dashboard.vue"
+
+
 export default {
   name: "Admin.vue",
   components: {Adm, Dashboard},
