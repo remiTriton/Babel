af00169c16d35c4f7e62cd47ba28e630f4ef0d67
triton <remi.triton@epitech.eu>
2021-11-02 16:55:36 +0100

    com

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -2,11 +2,13 @@
   <div>
     <div class="container mt-40">
   <div class="flex items-center text-black">
+  <button>
     <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mr-2 text-gray-600" fill="none" viewBox="0 0 24 24"
          stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
     </svg>
+  </button>
     <input  type="text"  name="name" placeholder="search..." v-model="searchQuery" 
            class="w-/10 py-2 border-b-2 border-gray-400 outline-none focus:border-green-400">
   </div>
