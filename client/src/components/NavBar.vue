f8738e8d8e869c823b2584ed89e9321b32424804
triton <remi.triton@epitech.eu>
2021-11-09 15:50:33 +0100

    maj form v1

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -5,7 +5,7 @@
     <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
     <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
   </div> -->
-      <router-link to="/WineList" >
+      <router-link to="/WineList.vue" >
         <img class="bb" src="../assets/arches.png" atl="" /></router-link>
       <div class="block lg:hidden">
         <button
@@ -34,22 +34,6 @@
       </div>
       <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
-
-          <router-link
-              to="/WineList"
-            class="
-              cat
-              block
-              mt-4
-              lg:inline-block
-              lg:mt-0
-              text-teal-200
-              hover:text-white
-              mr-4
-            "
-          >
-            Wine List
-          </router-link>
           <router-link
               to="/Blog"
             class="
