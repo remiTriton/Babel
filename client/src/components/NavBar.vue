c24ae789aae52a3d7394aed178b5b816258f7022
triton <remi.triton@epitech.eu>
2021-11-16 13:17:08 +0100

    logout

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-    <nav class="flexo flex  justify-between flex-wrap p-6">
+    <nav class="flexo flex justify-between flex-wrap p-6">
       <!-- <div class="flex items-center flex-shrink-0 text-white mr-6">
     <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
     <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
   </div> -->
-      <router-link to="/WineList" >
-        <img class="bb" src="../assets/arches.png" atl="" /></router-link>
+      <router-link
+        to="/Blog"
+        class="
+          flex
+          items-center
+          px-3
+          py-2
+          rounded
+          hover:text-white hover:border-white
+          text-2xl
+        "
+      >
+        Blog</router-link
+      >
       <div class="block lg:hidden">
         <button
           class="
@@ -18,8 +30,8 @@
             rounded
             text-teal-200
             border-teal-400
-            hover:text-white
-            hover:border-white
+            hover:text-white hover:border-white
+            text-4xl
           "
         >
           <svg
@@ -35,92 +47,24 @@
       <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
           <router-link
-              to="/Blog"
+            to="/wineList"
             class="
               cat
               block
               mt-4
-              lg:inline-block
-              lg:mt-0
+              ml-40
+              lg:inline-block lg:mt-0
               text-teal-200
               hover:text-white
             "
           >
-            Blog
+            <img class="bb" src="../assets/arches.png" atl="" />
           </router-link>
-        </div>
-        
-        <div>
-           <router-link
-            to="/login"
-            class="
-              cat
-              inline-block
-              text-sm
-              px-4
-              py-2
-              leading-none
-              border
-              rounded
-              text-white
-              border-white
-              hover:border-transparent
-              hover:text-teal-500
-              hover:bg-white
-              mt-4
-              lg:mt-0
-            "
-            >Login</router-link>
-        </div>
-        <div>
-           <router-link
-            to="/login"
-            class="
-              cat
-              inline-block
-              text-sm
-              px-4
-              py-2
-              leading-none
-              border
-              rounded
-              text-white
-              border-white
-              hover:border-transparent
-              hover:text-teal-500
-              hover:bg-whiteq
-              mt-4
-              lg:mt-0
-            "
-            >Logout</router-link>
         </div>
 
         <div>
           <router-link
-              to="/register"
-              class="
-              cat
-              inline-block
-              text-sm
-              px-4
-              py-2
-              leading-none
-              border
-              rounded
-              text-white
-              border-white
-              hover:border-transparent
-              hover:text-teal-500
-              hover:bg-white
-              mt-4
-              lg:mt-0
-            "
-          >Register</router-link>
-        </div>
-      </div>
-      <div>
-        <router-link
-            to="/Admin"
+            to="/login"
             class="
               cat
               inline-block
@@ -132,13 +76,77 @@
               rounded
               text-white
               border-white
-              hover:border-transparent
-              hover:text-teal-500
-              hover:bg-white
+              hover:border-transparent hover:text-teal-500 hover:bg-white
               mt-4
               lg:mt-0
             "
-        >Admin</router-link>
+            >Login</router-link
+          >
+        </div>
+        <div>
+          <router-link
+            to="/login"
+            class="
+              cat
+              inline-block
+              text-sm
+              px-4
+              py-2
+              leading-none
+              border
+              rounded
+              text-white
+              border-white
+              hover:border-transparent hover:text-teal-500 hover:bg-whiteq
+              mt-4
+              lg:mt-0
+            "
+            >Logout</router-link
+          >
+        </div>
+
+        <div>
+          <router-link
+            to="/register"
+            class="
+              cat
+              inline-block
+              text-sm
+              px-4
+              py-2
+              leading-none
+              border
+              rounded
+              text-white
+              border-white
+              hover:border-transparent hover:text-teal-500 hover:bg-white
+              mt-4
+              lg:mt-0
+            "
+            >Register</router-link
+          >
+        </div>
+      </div>
+      <div>
+        <router-link
+          to="/Admin"
+          class="
+            cat
+            inline-block
+            text-sm
+            px-4
+            py-2
+            leading-none
+            border
+            rounded
+            text-white
+            border-white
+            hover:border-transparent hover:text-teal-500 hover:bg-white
+            mt-4
+            lg:mt-0
+          "
+          >Admin</router-link
+        >
       </div>
     </nav>
   </div>
@@ -158,11 +166,9 @@ export default {
   width: 150px;
   height: 150px;
 }
-.cat{
+.cat {
   font-size: 30px;
-  display : flex;
+  display: flex;
   justify-content: space-between;
-
-
 }
 </style>
