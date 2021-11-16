55b5e994fd16efacbf13a089febc47b7183d1e43
triton <remi.triton@epitech.eu>
2021-11-16 12:59:42 +0100

    logout button

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -71,6 +71,28 @@
             "
             >Login</router-link>
         </div>
+        <div>
+           <router-link
+            to="/login"
+            class="
+              cat
+              inline-block
+              text-sm
+              px-4
+              py-2
+              leading-none
+              border
+              rounded
+              text-white
+              border-white
+              hover:border-transparent
+              hover:text-teal-500
+              hover:bg-white
+              mt-4
+              lg:mt-0
+            "
+            >Logout</router-link>
+        </div>
 
         <div>
           <router-link
