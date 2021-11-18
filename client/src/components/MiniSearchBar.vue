8a249603301b0ebb77acccbec77b9d9322993045
triton <remi.triton@epitech.eu>
2021-11-18 17:20:04 +0100

    :o

---
+++ client/src/components/MiniSearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  <input class="m-10 rounded-md" type="search">
+  <button class="rounded-md" type="search">
+  <svg  xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 " fill="none"  stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round"  d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg></button>
+  <input class="m-5" type="radio"> Commande
+  <input class="m-5" type="radio"> Ajout
+  </div>
+</template>
+
+<script>
+export default {
+ 
+name :"MiniSearchBar",
+
+}
+</script>
+
+<style>
+
+</style>
