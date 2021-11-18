0bc85188ba4d3993514d6faa823e0e51c537d62f
triton <remi.triton@epitech.eu>
2021-11-18 16:15:20 +0100

    iconic

--- client/src/components/UpdateOrder.vue
+++ client/src/components/UpdateOrder.vue
@@ -29,7 +29,7 @@
             >
               Cuvée
             </th>
-                  <th
+            <th
               scope="col"
               class="
                 px-6
@@ -57,15 +57,49 @@
             >
               Quantité
             </th>
+            <th>
+              <div>
+                <button
+                  v-if="!command"
+                  @click="nouveauBon(auth.user.email)"
+                  type="button"
+                  class="B
+                  px-6
+                py-3
+                text-center text-xs
+                font-medium
+                text-gray-500
+                uppercase
+                tracking-wider"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </th>
           </tr>
         </thead>
+
         <tbody v-for="wine in order.wines" :key="wine.id">
           <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
             <input v-model="wine.cuvee" />
           </td>
           <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            <input v-model="wine.couleur" /></td>
-            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+            <input v-model="wine.couleur" />
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
             <input v-model="wine.quantite" type="number" />
           </td>
           <td>
@@ -73,13 +107,27 @@
               @click.prevent="updateOrd(wine.id, wine.quantite)"
               class="text-black"
             >
-              Editer
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                />
+              </svg>
             </button>
           </td>
         </tbody>
       </table>
     </div>
-    <button class="text-black">Valider commande</button>
+    <button class="text-black">
+    Valider commande</button>
   </div>
 </template>
 
@@ -98,8 +146,7 @@ export default {
     async updateOrd(id, quantite) {
       await this.$store.dispatch("orders/updateOrder", [
         this.$route.params.id,
-        { wines:{
-        }},
+        { wines: {} },
       ]);
     },
   },
@@ -109,5 +156,14 @@ export default {
 <style scoped>
 * {
   color: gray;
+}
+
+.B {
+  color: #2a574c;
+}
+
+.round {
+  background-color: #2a574c;
+  color: white;
 }
 </style>
